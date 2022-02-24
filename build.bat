594c7a39d5d52b243fed51e0c3da5c60dd07e8c8
Lucas Henneke <70382336+zweek@users.noreply.github.com>
2022-02-24 14:10:14 +0100

    fixed CHI release build

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 mkdir export\vpk
-copy dir\englishclient_frontend.bsp.pak000_dir.vpk export\vpk
+copy dir\englishclient_frontend.bsp.pak000_dir.vpk export\vpk /y
 pushd export\vpk
 RSPNVPK englishclient_frontend.bsp.pak000_dir.vpk -s -d ..\..\src
 
@@ -19,15 +19,16 @@ if "%1"=="test" (
 if "%1"=="release" (
     cd ..
     7z a SRMM-v%2-ENG.zip vpk midimap.dll
-    cd ..\src\resource
-    move *.dat ../..
-    move *.txt ../..
-    cd ..\..\export\vpk
+    cd ..
+    move src\resource\*.dat .
+    move src\resource\*.txt .
+    copy dir\englishclient_frontend.bsp.pak000_dir.vpk export\vpk /y
+    cd export\vpk
     RSPNVPK englishclient_frontend.bsp.pak000_dir.vpk -s -d ..\..\src
     cd ..
     7z a SRMM-v%2-CHI.zip vpk midimap.dll
     popd
-    move *.dat src/resource
-    move *.txt src/resource
+    move *.dat src\resource
+    move *.txt src\resource
     echo Release builds done!
 )
